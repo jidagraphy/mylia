4dc7578668a47e660c097d8fa52df6672712bb7b
jidagraphy <jidagraphy@gmail.com>
2026-04-16 21:30:18 +0900

    disabled tool list

--- Tools/index.js
+++ Tools/index.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const { getConfig } = require('../Utility/config');
 
 const toolsDir = __dirname;
 
 const toolFiles = fs.readdirSync(toolsDir)
     .filter(f => f !== 'index.js' && f.endsWith('.js'));
+
+const disabled = new Set(getConfig()?.disabled_tools || []);
 
 const availableTools = {};
 const toolDeclarations = [];
@@ -13,6 +16,7 @@ for (const file of toolFiles) {
     const tool = require(path.join(toolsDir, file));
     // The new OpenAI schema stores the name under declaration.function.name 26.03.07
     const toolName = tool.declaration.function?.name || tool.declaration.name;
+    if (disabled.has(toolName)) continue;
     availableTools[toolName] = tool.handler;
     toolDeclarations.push(tool.declaration);
 }
